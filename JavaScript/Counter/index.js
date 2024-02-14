67237c0d7ecf0712db517b49e340c1efff9258a5
Cameron Norris <cameron.c.norris@gmail.com>
2024-02-14 06:43:29 -0600

    Half way done on counter program.

---
+++ JavaScript/Counter/index.js
@@ -0,0 +1,5 @@
+//A simple counter.
+
+const decreaseBtn = document.getElementById("decreaseBtn");
+const increaseBtn = document.getElementById("increaseBtn");
+const resetBtn = document.getElementById("resetBtn");
