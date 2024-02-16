935ebd12f8debde1e02643b17e1536ab17e5679e
Cameron Norris <cameron.c.norris@gmail.com>
2024-02-15 18:44:53 -0600

    Update index.js

--- JavaScript/Counter/index.js
+++ JavaScript/Counter/index.js
@@ -1,11 +1,12 @@
 //A simple counter.
 
+//Get the elements.
 const decreaseBtn = document.getElementById("decreaseBtn");
 const increaseBtn = document.getElementById("increaseBtn");
 const resetBtn = document.getElementById("resetBtn");
 const countLabel = document.getElementById("countLabel");
 let count = 0;
-
+//Event listeners.
 increaseBtn.onclick = function () {
     count++;
     countLabel.textContent = count;
